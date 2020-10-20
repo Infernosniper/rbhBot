02c73182c4c36f95d1d2b457e612858f69e34974
Infernosniper <72215581+Infernosniper@users.noreply.github.com>
2020-10-20 19:31:46 -0400

    Add files via upload

--- commands/abarbanel.js
+++ commands/abarbanel.js
@@ -2,13 +2,11 @@ module.exports = {
 	name: 'abarbanel',
 	description: 'Come learn the Abarbanel with me!',
 	execute(message, args, queue, serverQueue, client, animeBabesId, Discord){
-		const url = ['https://youtu.be/lZtoCWMsWp0']; //youtube link to rbh speaking
 		const command = 'play'; //we can't forward the command, because the original command was "abarbanel"
 		if(!(message.guild.id === animeBabesId)) return message.reply('Your server cannot use this command!');
-		client.commands.get('music').execute(command, message, url, queue, serverQueue, Discord); //uses the play command from music.js
 		
 		const embed = new Discord.MessageEmbed();
-		embed.setTitle('Come child, learn the Abarbanel with me')
+		embed.setTitle('Child, learn the Abarbanel!');
 		embed.setURL('https://www.sefaria.org/Abarbanel_on_Torah?lang=bi');
 		embed.setFooter('RBH is your eternal creator, never forget it.');
 		embed.setTimestamp();
